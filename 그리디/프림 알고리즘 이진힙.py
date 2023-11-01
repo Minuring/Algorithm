d151cea5e07a7ff46005db085e90deffe9575c2a
Minuring <mw9505@gmail.com>
2023-11-02 01:08:40 +0900

    [Add] 프림 알고리즘 : 이진힙 사용

---
+++ 그리디/프림 알고리즘 이진힙.py
@@ -0,0 +1,28 @@
+import heapq;
+import sys; input = sys.stdin.readline
+
+V, E = map(int, input().split()) #정점 수, 간선 수
+G = [[] for _ in range(V+1)]
+for _ in range(E) :
+  u, v, w = map(int, input().split())
+  G[u].append([v, w])
+  G[v].append([u, w])
+
+sum = 0
+visited = [False] * (V+1)
+
+heap = [[0,1]]
+
+while heap :
+
+    w,u = heapq.heappop(heap)
+    if visited[u] : continue
+
+    visited[u] = True
+    sum += w
+
+    # u에서 인접한 정점 탐색
+    for v,wt in G[u] :
+        heapq.heappush(heap, [wt,v])
+
+print(sum)
